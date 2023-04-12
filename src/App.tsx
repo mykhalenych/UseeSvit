0d0f8851b3a23451304cdfcf4fb30c8d0712eabe
mykhalenych <mykhalenych@gmail.com>
2023-04-12 17:00:01 +0300

    feature/initProject deleted duplicate import

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import {Provider} from 'react-redux';
 
 import store from './redux/store';
-import {SnackbarProvider} from 'notistack';
 import Theme from './Theme';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <SnackbarProvider maxSnack={3} anchorOrigin={{vertical: 'top', horizontal: 'right'}}>
-                <Theme />
-            </SnackbarProvider>
+            <Theme />
         </Provider>
     );
 };
