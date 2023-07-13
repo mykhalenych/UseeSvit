963f6b90853da62a2db6e8a9b4c41b50ff78bf63
Taras <baran.taras.dev@gmail.com>
2023-07-13 13:59:36 +0200

    feature/nextJS -update-i18n - changed i18n.ts from react to next

--- src/app/common/i18n.ts
+++ src/app/common/i18n.ts
@@ -1,19 +1,35 @@
-/* eslint-disable import/no-named-as-default-member */
-
 import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import {initReactI18next} from 'react-i18next';
 
-i18n.use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
+import TranslationEn from '../../../public/locales/en/translation.json';
+import TranslationUa from '../../../public/locales/ua/translation.json';
+import NotificationEn from '../../../public/locales/en/notification.json';
+import NotificationUa from '../../../public/locales/en/notification.json';
+import ValidationUa from '../../../public/locales/en/validation.json';
+import ValidationEn from '../../../public/locales/en/validation.json';
+
+i18n
+    .use(Backend)
+    .use(initReactI18next).init({
+        lng: 'en',
         fallbackLng: 'en',
         debug: false,
         interpolation: {
             escapeValue: false,
         },
-    });
+        resources: {
+            en: {
+            translation: TranslationEn,
+            notification: NotificationEn,
+            validation: ValidationUa,
+            },
+            ua: {
+            translation: TranslationUa,
+            notification: NotificationUa,
+            validation: ValidationEn,
+            }
+        },
+});
 
 export default i18n;
