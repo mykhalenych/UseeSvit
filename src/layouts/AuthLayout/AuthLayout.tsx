6b11e82b11072a6c4e83cc2ab8a7d51dd7341d6c
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-07-13 15:44:26 +0300

    bag/focusIssue Added token to Auth

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Outlet} from 'react-router-dom';
 
 import Navigation from '../../components/common/Navigation/Navigation';
 import logInImage from '../../assets/img/logInImage.png';
 
 import {ContainerDiv} from './Styles';
+import {fetchUser} from '../../redux/auth/thunks';
+import {useAppDispatch} from '../../redux/store';
+import {getAuthTokens} from '../../common/utils';
 
 const AuthLayout = () => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const token = getAuthTokens();
+
+        if (token) {
+            dispatch(fetchUser());
+        }
+    }, [dispatch]);
     return (
         <>
             <Navigation />
