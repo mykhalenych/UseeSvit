e81f84c52077272102f12c9a0ba39ad674aac99a
Andriy Nosov <nosovandriy@gmail.com>
2023-07-14 01:55:23 +0300

    feature/add-sign-up-page add sign-up page

--- src/Routes/constants.ts
+++ src/Routes/constants.ts
@@ -1,7 +1,7 @@
 export const AUTH_PATH = '/auth';
 export const LOGIN_PATH = `${AUTH_PATH}/login`;
 export const FORGOT_PATH = `${AUTH_PATH}/forgot-password`;
-export const SIGN_IN_PATH = `${AUTH_PATH}/sign-in`;
+export const SIGN_UP_PATH = `${AUTH_PATH}/sign-up`;
 export const ACTIVATION_PATH = `${AUTH_PATH}/activation/:activationToken`;
 export const CHECK_EMAIL_PATH = `${AUTH_PATH}/check-email`;
 export const RESET_PASSWORD_PATH = `${AUTH_PATH}/reset-password/:resetToken`;
---
+++ src/app/auth/sign-up/types.ts
@@ -0,0 +1,5 @@
+export interface ISignInProps {
+    email: string;
+    password: string;
+    name: string;
+}
